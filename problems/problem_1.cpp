36e143c6c4fb16ad249738a581dc94395d8d1bcf
UlugKh <141512061+UlugKh@users.noreply.github.com>
2023-11-13 20:15:07 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,25 +1,23 @@
 void problemSolution1(float consumed_water) {
-    float cost;
-    // write your code here
     float cost = 13;
-    if (consumption > 30) {
+    if (consumed_water > 30) {
         cost += 30 * 0.4;
-        consumption -= 30;
-        if (consumption > 20) {
+        consumed_water -= 30;
+        if (consumed_water > 20) {
             cost += 20 * 0.12;
-            consumption -= 20;
-            if (consumption > 10) {
+            consumed_water -= 20;
+            if (consumed_water > 10) {
                 cost += 10 * 1.4;
-                consumption -= 10;
-                cost += consumption * 1.5;
+                consumed_water -= 10;
+                cost += consumed_water * 1.5;
             } else {
-                cost += consumption * 1.4;
+                cost += consumed_water * 1.4;
             }
         } else {
-            cost += consumption * 0.12;
+            cost += consumed_water * 0.12;
         }
     } else {
-        cost += consumption * 0.4;
+        cost += consumed_water * 0.4;
     }
     return cost;
 }

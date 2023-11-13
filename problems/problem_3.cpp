14927e370b7d0208bc9ec1af8b7adf5bfc616fd7
UlugKh <141512061+UlugKh@users.noreply.github.com>
2023-11-13 22:24:14 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,7 +2,25 @@
 
 std::string problemSolution3(float height, char S) {
     // write your code here
-
+     if (sex == 'M') {
+        if (height < 1.6) {
+            return "Short";
+        } else if (height < 1.8) {
+            return "Average";
+        } else {
+            return "Tall";
+        }
+    } else if (sex == 'F') {
+        if (height < 1.5) {
+            return "Short";
+        } else if (height < 1.7) {
+            return "Average";
+        } else {
+            return "Tall";
+        }
+    } else {
+        return "Invalid input";
+    }
     // use return to return your result
     // make use of control flow statements
 }

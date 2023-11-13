b0d3a0dc1514ec08305eab567da41a02833a47f2
UlugKh <141512061+UlugKh@users.noreply.github.com>
2023-11-13 22:43:54 +0500

    Update problem_4.cpp

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -1,9 +1,25 @@
+#include <iostream>
 #include <string>
 #include <sstream>
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
+    std::istringstream iss(macAddress);
+    int firstOctet;
 
+    if (!(iss >> std::hex >> firstOctet)) {
+        return "Invalid MAC address format";
+    }
+
+    if (firstOctet == 0xFF) {
+        return "Broadcast";
+    }
+
+    if (firstOctet % 2 == 0) {
+        return "Unicast";
+    } else {
+        return "Multicast";
+    }
     // make use of control flow statements
     // return result;
 }

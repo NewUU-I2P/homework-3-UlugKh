c0556ca2d54135df40862ea0b7e5d9102d4ea4b7
UlugKh <141512061+UlugKh@users.noreply.github.com>
2023-11-13 22:53:23 +0500

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,6 +1,12 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
    // write your code here
-
+       switch (operation) {
+        case '+': return x + y;
+        case '-': return x - y;
+        case '*': return x * y;
+        case '/': return (y != 0) ? x / y : (cout << "Error!" << endl, 1);
+        default: cout << "Error!" << endl; return 1;
+    }
    return result;
 }

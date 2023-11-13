e0c4a700b865e586f89ff785ebe04ab2875820cd
UlugKh <141512061+UlugKh@users.noreply.github.com>
2023-11-13 23:12:02 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,6 +1,6 @@
 float problemSolution2(float i, float j, float k) {
     float result;
     // write your code here;
-    result = (p1 > p2) ? ((p1 > p3) ? p1 : p3) : (p2 > p3 ? p2 : p3);
+    result = (i > j) ? ((i > k) ? i : k) : (j > k ? j : k);
     return result;
 }
